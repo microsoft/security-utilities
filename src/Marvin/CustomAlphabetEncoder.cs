ce5c10faad6708c980029a226e0e98732692c524
marmegh <30842915+marmegh@users.noreply.github.com>
2022-01-10 12:55:45 -0800

    Adding Custom Alphabet Encoder Class (#5)
    * Added CustomAlphabetEncoder class
    
    * Added CustomAlphabetEncoder tests
    
    * Added tests for NullArgumentExceptions and fluent assertion updates.
    
    * Fixed whitespace.
    
    * Added validation and test for alphabet used to instantiate CustomAlphabetEncoder to improve encode/decode reliability.
    
    * addressed static variable, whitespace, and byte array feedback
    
    * Removed unneeded comments and additional test cleanup.
    
    * Limited valid characters for encoder and updated unit tests accordingly.
    
    * updated to test all alphabet options
    
    * renamed variable for clarity
    
    * removed unnecessary variable
    
    * Updates for alphabet and exception clarification.
    
    * Addressed spacing feedback
    
    * Fixed test cases for versions other than .NET 452
    
    * Fixed string generation for exception
    
    * Added spaces before parens

---
+++ src/Marvin/CustomAlphabetEncoder.cs
@@ -0,0 +1,109 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Microsoft.Security.Utilities
+{
+    /// <summary>
+    /// Custom encoder class.
+    /// </summary>
+    public class CustomAlphabetEncoder
+    {
+        internal const string DefaultBase62Alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+        [ThreadStatic]
+        private static StringBuilder s_sb;
+
+        private string alphabet;
+        private Dictionary<char, uint> charToValueMap;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CustomAlphabetEncoder"/> class.
+        /// </summary>
+        /// <param name="customAlphabet">The alphabet to be used for all encoding/decoding operations. Must consist of nonwhitespace ASCII letters, numbers, and/or punctuation.</param>
+        /// <exception cref="ArgumentException">customAlphabet contains a duplicate or forbidden character.</exception>
+        public CustomAlphabetEncoder(string customAlphabet = DefaultBase62Alphabet)
+        {
+            alphabet = string.IsNullOrWhiteSpace(customAlphabet) ? DefaultBase62Alphabet : customAlphabet;
+
+            if (alphabet.Length < 2)
+            {
+                throw new ArgumentException(nameof(customAlphabet), "Alphabet must be at least 2 characters.");
+            }
+
+            charToValueMap = new Dictionary<char, uint>();
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                // Repeated values in the custom alphabet will cause unreliable encoding/decoding.
+                if (charToValueMap.ContainsKey(alphabet[i]))
+                {
+                    throw new ArgumentException(nameof(customAlphabet), "Duplicate value detected in the alphabet.");
+                }
+
+                if (Char.IsWhiteSpace(alphabet[i]) || Char.IsSurrogate(alphabet[i]) || (int)alphabet[i] > 127)
+                {
+                    throw new ArgumentException(nameof(customAlphabet), $"Forbidden character type detected in the alphabet: {alphabet[i]}.");
+                }
+
+                charToValueMap[alphabet[i]] = (uint)i;
+            }
+        }
+
+        /// <summary>
+        /// Encode an unsigned integer (a checksum) in a given character set.
+        /// </summary>
+        /// <param name="data">The unsigned integer to encode.</param>
+        /// <returns>
+        /// The unsigned integer encoded using the character set with which this class was instantiated.
+        /// </returns>
+        public string Encode(uint data)
+        {
+            s_sb ??= new StringBuilder();
+            s_sb.Clear();
+
+            while (data > 0)
+            {
+                s_sb.Append(alphabet[(int)(data % (uint)alphabet.Length)]);
+                data /= (uint)alphabet.Length;
+            }
+
+            return new string(s_sb.ToString().Reverse().ToArray());
+        }
+
+        /// <summary>
+        /// Decode a byte array from a given character set.
+        /// </summary>
+        /// <param name="encodedValue">The encoded byte array to decode.</param>
+        /// <returns>
+        /// The byte array encoded using the character set wit which this class was instantiated.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="encodedValue"/> is null.</exception>
+        public byte[] Decode(string encodedValue)
+        {
+            if (encodedValue == null)
+            {
+                throw new ArgumentNullException(nameof(encodedValue));
+            }
+
+            uint decodedValue = 0;
+            uint alphabetLength = (uint)alphabet.Length;
+
+            foreach (char c in encodedValue)
+            {
+                if (!charToValueMap.ContainsKey(c))
+                {
+                    throw new ArgumentException(nameof(encodedValue), "Alphabet does not contain all characters in input");
+                }
+
+                decodedValue *= alphabetLength;
+                decodedValue += charToValueMap[c];
+            }
+
+            return BitConverter.GetBytes(decodedValue);
+        }
+    }
+}
---
+++ src/Tests.Marvin/CustomAlphabetEncoderTests.cs
@@ -0,0 +1,224 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Security.Utilities
+{
+
+    [TestClass]
+    public class CustomAlphabetEncoderTests
+    {
+        [TestMethod]
+        public void CustomAlphabetEncoder_EncodeUintWithTestCases()
+        {
+            Random random = new Random();
+
+            var testCases = new[]
+            {
+                new {
+                    Alphabet = String.Empty,
+                    Input = uint.MinValue,
+                    ExpectedEncoded = ""
+                },
+                new
+                {
+                    Alphabet = String.Empty,
+                    Input = uint.MaxValue,
+                    ExpectedEncoded = "4gfFC3"
+                },
+                new
+                {
+                    Alphabet = String.Empty,
+                    Input = (uint)2883656711,
+                    ExpectedEncoded = "399Wq7"
+                },
+                new
+                {
+                    Alphabet = "abc",
+                    Input = (uint)2883656711,
+                    ExpectedEncoded = "cbbaccccaacccbbaabac"
+                },
+                new
+                {
+                    Alphabet = "abcdef",
+                    Input = UInt32.MinValue,
+                    ExpectedEncoded = ""
+                },
+                new
+                {
+                    Alphabet = "abcdef",
+                    Input = (uint)123456,
+                    ExpectedEncoded = "cdfbdca"
+                },
+            };
+
+            foreach (var testCase in testCases)
+            {
+                var testEncoder = new CustomAlphabetEncoder(testCase.Alphabet);
+                string actualEncoded = testEncoder.Encode(testCase.Input);
+
+                byte[] actualDecoded = testEncoder.Decode(actualEncoded);
+                uint actualDecodedUint = BitConverter.ToUInt32(actualDecoded, 0);
+
+                actualEncoded.Should().Be(testCase.ExpectedEncoded);
+                actualDecodedUint.Should().Be(testCase.Input);
+            }
+        }
+
+        [TestMethod]
+        public void CustomAlphabetEncoder_ShouldEncodeAndDecodeValidAlphabets()
+        {
+            var random = new Random();
+
+            var testInputs = new List<uint>
+            {
+                uint.MinValue,
+                uint.MaxValue,
+                (uint)random.Next(),
+                (uint)random.Next(),
+                (uint)random.Next()
+            };
+
+            foreach(string alphabet in GenerateValidAlphabetTestCases())
+            {
+                var testEncoder = new CustomAlphabetEncoder(alphabet);
+
+                foreach (uint input in testInputs)
+                {
+                    string actualEncoded = testEncoder.Encode(input);
+
+                    byte[] actualDecoded = testEncoder.Decode(actualEncoded);
+                    uint actualDecodedUint = BitConverter.ToUInt32(actualDecoded, 0);
+
+                    actualDecodedUint.Should().Be(input);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void CustomAlphabetEncoder_ShouldThrowInvalidArgumentExceptionIfAlphabetIsInvalid()
+        {
+            var testCases = new[]
+            {
+                new {
+                    Alphabet = "a",
+                    ExpectedMessage = "Alphabet must be at least 2 characters."
+                },
+                new {
+                    Alphabet = "aaa",
+                    ExpectedMessage = "Duplicate value detected in the alphabet."
+                },
+                new {
+                    Alphabet = GenerateAsciiString(),
+                    ExpectedMessage = "Forbidden character type detected in the alphabet"
+                },
+                new {
+                    Alphabet = "abc\uD800",
+                    ExpectedMessage = "Forbidden character type detected in the alphabet"
+                },
+                new {
+                    Alphabet = "abc\uDC00",
+                    ExpectedMessage = "Forbidden character type detected in the alphabet"
+                },
+                new {
+                    Alphabet = GenerateAsciiString(130,132),
+                    ExpectedMessage = "Forbidden character type detected in the alphabet"
+                },
+                new {
+                    Alphabet = "₼abc",
+                    ExpectedMessage = "Forbidden character type detected in the alphabet"
+                },
+                new {
+                    Alphabet = "ﺀabc",
+                    ExpectedMessage = "Forbidden character type detected in the alphabet"
+                },
+                new {
+                    Alphabet = "∞abc",
+                    ExpectedMessage = "Forbidden character type detected in the alphabet"
+                },
+                new {
+                    Alphabet = "µabc",
+                    ExpectedMessage = "Forbidden character type detected in the alphabet"
+                },
+            };
+
+            foreach(var testCase in testCases)
+            {
+                Action action = () => new CustomAlphabetEncoder(testCase.Alphabet);
+
+                action.Should().Throw<ArgumentException>().Where(e => e.Message.Contains(testCase.ExpectedMessage));
+            }
+        }
+
+        [TestMethod]
+        public void CustomAlphabetEncoder_ShouldThrowExceptionWithNullInputOnDecode()
+        {
+            var testEncoder = new CustomAlphabetEncoder("abc");
+            string data = null;
+
+            Action decodeAction = () => testEncoder.Decode(data);
+            decodeAction.Should().Throw<ArgumentNullException>();
+        }
+
+        [TestMethod]
+        public void CustomAlphabetEncoder_ShouldThrowExceptionIfDecodedCharNotInAlphabet()
+        {
+            string alphabet = "abc";
+            string encodedInput = "123";
+            var testEncoder = new CustomAlphabetEncoder(alphabet);
+
+            Action decodeAction = () => testEncoder.Decode(encodedInput);
+            decodeAction.Should().Throw<ArgumentException>();
+        }
+
+        [TestMethod]
+        public void CustomAlphabetEncoder_VerifyStaticAndInstanceDate()
+        {
+            var testEncoder1 = new CustomAlphabetEncoder("abcdef");
+            uint randomChecksum = uint.MaxValue - 1000;
+
+            string encodedChecksum1 = testEncoder1.Encode(randomChecksum);
+
+            var testEncoder2 = new CustomAlphabetEncoder("abdefghijklmnopqrstuv");
+
+            string encodedChecksum2 = testEncoder1.Encode(randomChecksum);
+
+            encodedChecksum1.Should().Be(encodedChecksum2);
+        }
+
+        private IEnumerable<string> GenerateValidAlphabetTestCases()
+        {
+            var output = new List<string>
+            {
+                String.Empty,
+            };
+
+            // generate a sample of random test alphabets
+            for (int i = 2; i < 94; i++)
+            {
+                output.Add(GenerateAsciiString(33, (33 + i)));
+            }
+
+            return output;
+        }
+
+        private string GenerateAsciiString(int low = 0, int high = 256)
+        {
+            var sb = new StringBuilder();
+
+            for (int i = low; i < high; i++)
+            {
+                sb.Append((char)i);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
