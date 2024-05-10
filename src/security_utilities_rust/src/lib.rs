9ec4c9b7b3818ef35563b0bf79e20ab73d91c16b
Suvam Mukherjee <sumukherjee@microsoft.com>
2024-05-10 13:10:43 -0400

    Adding c3id support in Rust (#9)
    * adding c3id rust support
    
    * updating comments

--- src/security_utilities_rust/src/lib.rs
+++ src/security_utilities_rust/src/lib.rs
@@ -1,5 +1,8 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-pub mod marvin;
-pub mod identifiable_secrets;
+mod microsoft_security_utilities_core;
+
+mod marvin_tests;
+mod identifiable_secrets_tests;
+mod cross_company_correlating_id_tests;
---
+++ src/security_utilities_rust/src/microsoft_security_utilities_core/cross_company_correlating_id.rs
@@ -0,0 +1,36 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+use base64::{engine::general_purpose::STANDARD, Engine as _};
+use lazy_static::lazy_static;
+use sha2::{Sha256, Digest};
+use std::fmt;
+use std::cell::RefCell;
+
+thread_local! {
+    static THREAD_LOCAL_SHA256: RefCell<Sha256> = RefCell::new(Sha256::new());
+}
+
+pub fn generate_cross_company_correlating_id(text: &str) -> String {
+    let hash = generate_sha256_hash(text);
+
+    let hash = format!("CrossMicrosoftCorrelatingId:{}", hash);
+
+    let checksum = THREAD_LOCAL_SHA256.with(|sha| {
+        sha.borrow_mut().update(hash.as_bytes());
+        sha.borrow_mut().finalize_reset()
+    });
+
+    let to_encode = &checksum[0..15];
+    STANDARD.encode(to_encode)
+}
+
+pub fn generate_sha256_hash(text: &str) -> String {
+
+    let result = THREAD_LOCAL_SHA256.with(|sha| {
+        sha.borrow_mut().update(text.as_bytes());
+        sha.borrow_mut().finalize_reset()
+    });
+
+    fmt::format(format_args!("{:X}", result))
+}
