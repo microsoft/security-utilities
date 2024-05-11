7c3ed02f0503734762c2bacbdd2d6c39ad49ed38
Beau Belgrave <beaub@linux.microsoft.com>
2024-05-11 00:59:11 +0000

    His/Rust: Expose Scan and ScanOptions for FFI
    Scan and ScanOptions are behind a non-public module.
    
    Ensure we have both Scan and ScanOptions exported for callers to
    utilize.
    
    Signed-off-by: Beau Belgrave <beaub@linux.microsoft.com>

--- src/security_utilities_rust/src/lib.rs
+++ src/security_utilities_rust/src/lib.rs
@@ -6,3 +6,6 @@ mod microsoft_security_utilities_core;
 mod marvin_tests;
 mod identifiable_secrets_tests;
 mod cross_company_correlating_id_tests;
+
+pub type IdentifiableScan = microsoft_security_utilities_core::identifiable_scans::Scan;
+pub type IdentifiableScanOptions = microsoft_security_utilities_core::identifiable_scans::ScanOptions;
