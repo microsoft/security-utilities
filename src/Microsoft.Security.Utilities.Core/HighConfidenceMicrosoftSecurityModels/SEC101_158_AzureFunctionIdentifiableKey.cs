ac7eaa3dd5c7156cb44f2e9668e1af2dc7a763f0
Michael C. Fanning <mikefan@microsoft.com>
2024-05-15 09:36:02 -0700

    Add new rule classes.

---
+++ src/Microsoft.Security.Utilities.Core/HighConfidenceMicrosoftSecurityModels/SEC101_158_AzureFunctionIdentifiableKey.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+
+namespace Microsoft.Security.Utilities
+{
+    internal class AzureFunctionIdentifiableKey : IdentifiableKey
+    {
+        public AzureFunctionIdentifiableKey()
+        {
+            Id = "SEC101/158";
+            Name = nameof(AzureContainerRegistryIdentifiableKey);
+        }
+
+        public override bool EncodeForUrl => true;
+
+        override public string Signature => IdentifiableMetadata.AzureFunctionSignature;
+
+        override public IEnumerable<ulong> ChecksumSeeds => new[]
+        {
+            IdentifiableMetadata.AzureFunctionKeyChecksumSeed,
+            IdentifiableMetadata.AzureFunctionMasterKeyChecksumSeed,
+            IdentifiableMetadata.AzureFunctionSystemKeyChecksumSeed,
+        };
+
+        public override string Pattern
+        {
+            get => @$"{WellKnownRegexPatterns.PrefixUrlSafeBase64}(?<refine>[{WellKnownRegexPatterns.RegexEncodedUrlSafeBase64}]{{44}}{Signature}[{WellKnownRegexPatterns.RegexEncodedUrlSafeBase64}]{{5}}[AQgw]==){WellKnownRegexPatterns.SuffixUrlSafeBase64}";
+            protected set => base.Pattern = value;
+        }
+
+        override public uint KeyLength => 40;
+    }
+}
