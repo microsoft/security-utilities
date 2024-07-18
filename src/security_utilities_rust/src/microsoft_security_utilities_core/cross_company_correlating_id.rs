b0bfccb0acd6ea17337951b98f8b7f45a4b280b7
Johannes Cornelis Draaijer <jdraaijer@microsoft.com>
2024-07-18 09:23:53 +0200

    fix(cross_company_correlating_id): Decrease allocations & formatting calls during generation of c3id
    There are some steps in the generation of the Cross Company Correlation ID
    for which we know the amount and type of data that we will be processing at
    compile time. Passing this knowledge directly to those functions decreases
    the amount of time spent formatting & reallocating at runtime.

--- src/security_utilities_rust/src/microsoft_security_utilities_core/cross_company_correlating_id.rs
+++ src/security_utilities_rust/src/microsoft_security_utilities_core/cross_company_correlating_id.rs
@@ -2,9 +2,9 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 use base64::{engine::general_purpose::STANDARD, Engine as _};
-use sha2::{Sha256, Digest};
-use std::fmt;
+use sha2::{Digest, Sha256};
 use std::cell::RefCell;
+use std::fmt::Write;
 
 thread_local! {
     static THREAD_LOCAL_SHA256: RefCell<Sha256> = RefCell::new(Sha256::new());
@@ -13,23 +13,25 @@ thread_local! {
 pub fn generate_cross_company_correlating_id(text: &str) -> String {
     let hash = generate_sha256_hash(text);
 
-    let hash = format!("CrossMicrosoftCorrelatingId:{}", hash);
-
     let checksum = THREAD_LOCAL_SHA256.with(|sha| {
-        sha.borrow_mut().update(hash.as_bytes());
-        sha.borrow_mut().finalize_reset()
+        let mut sha = sha.borrow_mut();
+
+        sha.update("CrossMicrosoftCorrelatingId:");
+        sha.update(hash);
+        sha.finalize_reset()
     });
 
     let to_encode = &checksum[0..15];
     STANDARD.encode(to_encode)
 }
 
-pub fn generate_sha256_hash(text: &str) -> String {
-
+fn generate_sha256_hash(text: &str) -> String {
     let result = THREAD_LOCAL_SHA256.with(|sha| {
         sha.borrow_mut().update(text.as_bytes());
         sha.borrow_mut().finalize_reset()
     });
 
-    fmt::format(format_args!("{:X}", result))
+    let mut output = String::with_capacity(2 * 32);
+    write!(output, "{:X}", result).unwrap();
+    output
 }
