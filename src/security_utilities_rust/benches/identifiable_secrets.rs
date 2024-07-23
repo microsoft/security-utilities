0eda1bb38a9872480e3c33757de7ef2327175421
Johannes Cornelis Draaijer <jdraaijer@microsoft.com>
2024-07-23 15:51:35 +0200

    Rust/Bench: add secret masking benchmarks

--- src/security_utilities_rust/benches/identifiable_secrets.rs
+++ src/security_utilities_rust/benches/identifiable_secrets.rs
@@ -1,13 +1,19 @@
 use std::time::Duration;
 
-use criterion::{criterion_group, criterion_main, Criterion};
-use microsoft_security_utilities_core::microsoft_security_utilities_core::identifiable_secrets::{
-    generate_common_annotated_key, try_validate_common_annotated_key,
+use criterion::{criterion_group, criterion_main, Bencher, Criterion};
+use microsoft_security_utilities_core::microsoft_security_utilities_core::{
+    identifiable_scans::{Scan, ScanOptions},
+    identifiable_secrets::{
+        generate_common_annotated_key, try_validate_common_annotated_key, SecretMasker,
+    },
 };
 
 fn id_secrets_benchmark(c: &mut Criterion) {
-    let data = generate_common_annotated_key(
-        "ABCD",
+    let valid_signature = "ABCD";
+
+    // generate a key
+    let valid_key = generate_common_annotated_key(
+        valid_signature,
         true,
         Some(&vec![0; 9]),
         Some(&vec![0; 3]),
@@ -17,8 +23,37 @@ fn id_secrets_benchmark(c: &mut Criterion) {
     .unwrap();
 
     c.bench_function("validate", |b| {
-        b.iter(|| try_validate_common_annotated_key(&data, "ABCD"))
+        b.iter(|| try_validate_common_annotated_key(&valid_key, "ABCD"))
     });
+
+    let mut masking_group = c.benchmark_group("mask_secrets");
+    let masking_input = include_str!("../test_files/bench_data.utf8");
+
+    let mask_secrets = |b: &mut Bencher, token, validate| {
+        let options = ScanOptions::default();
+        let mut secret_masker = SecretMasker {
+            scan: Scan::new(options),
+        };
+
+        b.iter(|| {
+            let mut input = masking_input.to_string();
+            secret_masker.mask_secrets(&mut input, token, validate);
+        });
+    };
+
+    masking_group
+        .bench_function("mask_secrets (validate without default)", |b| {
+            mask_secrets(b, None, true)
+        })
+        .bench_function("mask_secrets (don't validate without default)", |b| {
+            mask_secrets(b, None, false)
+        })
+        .bench_function("mask_secrets (validate with default)", |b| {
+            mask_secrets(b, Some("abcdefghijkl"), true)
+        })
+        .bench_function("mask_secrets (don't validate with default)", |b| {
+            mask_secrets(b, Some("abcdefghijkl"), false)
+        });
 }
 
 criterion_group! {
