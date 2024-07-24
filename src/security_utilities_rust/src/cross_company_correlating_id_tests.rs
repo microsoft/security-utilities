efb8c45c94e5988e525323b0b8c0e9fd49fe9516
Johannes Cornelis Draaijer <jdraaijer@microsoft.com>
2024-07-24 09:52:37 +0200

    fix(cross_company_correlating_id): remove final allocation
    Thanks to a comment by @suvamM I realized that there is no need to
    allocate the string data at all. Since we know the length of the data
    up front, and the formatting we do is so basic that doing it by hand
    works fine, we can remove the allocation and gain another 7% speedup.

--- src/security_utilities_rust/src/cross_company_correlating_id_tests.rs
+++ src/security_utilities_rust/src/cross_company_correlating_id_tests.rs
@@ -1,16 +1,53 @@
-#[cfg(test)]
+#![cfg(test)]
+
+use microsoft_security_utilities_core::cross_company_correlating_id::hex_encode;
+
 use super::*;
 
 /// Compare a Rust generated C3Id against a well-known test case from C#.
 #[test]
-fn validate_cross_company_correlation_id() {    
+fn validate_cross_company_correlation_id() {
     // Assume
     let secret = "test";
     let expected_c3id = "rPHgxCVAOw6CZsT9xXEw";
 
     // Act
     let actual_c3id: String = microsoft_security_utilities_core::cross_company_correlating_id::generate_cross_company_correlating_id(secret);
-    
+
     // Assert
     assert_eq!(expected_c3id, actual_c3id);
+}
+
+#[test]
+fn hex_valid() {
+    let res = [
+        (0, b'0'),
+        (1, b'1'),
+        (2, b'2'),
+        (3, b'3'),
+        (4, b'4'),
+        (5, b'5'),
+        (6, b'6'),
+        (7, b'7'),
+        (8, b'8'),
+        (9, b'9'),
+        (10, b'A'),
+        (11, b'B'),
+        (12, b'C'),
+        (13, b'D'),
+        (14, b'E'),
+        (15, b'F'),
+    ];
+
+    for (input, output) in res {
+        let hex = hex_encode(input).unwrap();
+        assert_eq!(hex, output, "{}, {}, {}", input, output, hex as char);
+    }
+}
+
+#[test]
+fn hex_invalid() {
+    for v in 16..u8::MAX {
+        assert!(hex_encode(v).is_none());
+    }
 }
--- src/security_utilities_rust/src/microsoft_security_utilities_core/cross_company_correlating_id.rs
+++ src/security_utilities_rust/src/microsoft_security_utilities_core/cross_company_correlating_id.rs
@@ -4,14 +4,14 @@
 use base64::{engine::general_purpose::STANDARD, Engine as _};
 use sha2::{Digest, Sha256};
 use std::cell::RefCell;
-use std::fmt::Write;
 
 thread_local! {
     static THREAD_LOCAL_SHA256: RefCell<Sha256> = RefCell::new(Sha256::new());
 }
 
 pub fn generate_cross_company_correlating_id(text: &str) -> String {
-    let hash = generate_sha256_hash(text);
+    let mut buffer = [0u8; 64];
+    let hash = generate_sha256_hash(text, &mut buffer);
 
     let checksum = THREAD_LOCAL_SHA256.with(|sha| {
         let mut sha = sha.borrow_mut();
@@ -25,13 +25,32 @@ pub fn generate_cross_company_correlating_id(text: &str) -> String {
     STANDARD.encode(to_encode)
 }
 
-fn generate_sha256_hash(text: &str) -> String {
+fn generate_sha256_hash<'a>(text: &str, buffer: &'a mut [u8; 64]) -> &'a [u8; 64] {
     let result = THREAD_LOCAL_SHA256.with(|sha| {
         sha.borrow_mut().update(text.as_bytes());
         sha.borrow_mut().finalize_reset()
     });
 
-    let mut output = String::with_capacity(2 * 32);
-    write!(output, "{:X}", result).unwrap();
-    output
+    let data: [u8; 32] = result.into();
+
+    for (idx, byte) in data.into_iter().enumerate() {
+        // NOTE(unwrap): u8 & 0xF is always valid.
+        let lhs = hex_encode((byte >> 4) & 0xF).unwrap();
+        let rhs = hex_encode(byte & 0xF).unwrap();
+
+        buffer[idx * 2] = lhs;
+        buffer[(idx * 2) + 1] = rhs;
+    }
+
+    buffer
+}
+
+pub(crate) fn hex_encode(value: u8) -> Option<u8> {
+    if value < 10 {
+        Some(value.wrapping_add(b'0'))
+    } else if value < 16 {
+        Some(value.wrapping_sub(10).wrapping_add(b'A'))
+    } else {
+        None
+    }
 }
