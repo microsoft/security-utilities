20d52cad3e816245e1b248968e417582401626e1
jdraaijer-microsoft <165163583+jdraaijer-microsoft@users.noreply.github.com>
2024-07-30 18:20:33 +0200

    Rust/c3id: Decrease allocations & formatting (#74)
    * fix(cross_company_correlating_id): Decrease allocations & formatting calls during generation of c3id
    
    There are some steps in the generation of the Cross Company Correlation ID
    for which we know the amount and type of data that we will be processing at
    compile time. Passing this knowledge directly to those functions decreases
    the amount of time spent formatting & reallocating at runtime.
    
    * fix(cross_company_correlating_id): remove final allocation
    
    Thanks to a comment by @suvamM I realized that there is no need to
    allocate the string data at all. Since we know the length of the data
    up front, and the formatting we do is so basic that doing it by hand
    works fine, we can remove the allocation and gain another 7% speedup.
    
    * fix(cross_company_correlating_id): borrow only once
    
    * fix(cross_company_correlating_id): document hex encoding

--- src/security_utilities_rust/src/microsoft_security_utilities_core/cross_company_correlating_id.rs
+++ src/security_utilities_rust/src/microsoft_security_utilities_core/cross_company_correlating_id.rs
@@ -2,8 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 use base64::{engine::general_purpose::STANDARD, Engine as _};
-use sha2::{Sha256, Digest};
-use std::fmt;
+use sha2::{Digest, Sha256};
 use std::cell::RefCell;
 
 thread_local! {
@@ -11,25 +10,52 @@ thread_local! {
 }
 
 pub fn generate_cross_company_correlating_id(text: &str) -> String {
-    let hash = generate_sha256_hash(text);
-
-    let hash = format!("CrossMicrosoftCorrelatingId:{}", hash);
+    let mut buffer = [0u8; 64];
+    let hash = generate_sha256_hash(text, &mut buffer);
 
     let checksum = THREAD_LOCAL_SHA256.with(|sha| {
-        sha.borrow_mut().update(hash.as_bytes());
-        sha.borrow_mut().finalize_reset()
+        let mut sha = sha.borrow_mut();
+
+        sha.update("CrossMicrosoftCorrelatingId:");
+        sha.update(hash);
+        sha.finalize_reset()
     });
 
     let to_encode = &checksum[0..15];
     STANDARD.encode(to_encode)
 }
 
-pub fn generate_sha256_hash(text: &str) -> String {
-
+fn generate_sha256_hash<'a>(text: &str, buffer: &'a mut [u8; 64]) -> &'a [u8; 64] {
     let result = THREAD_LOCAL_SHA256.with(|sha| {
-        sha.borrow_mut().update(text.as_bytes());
-        sha.borrow_mut().finalize_reset()
+        let mut sha = sha.borrow_mut();
+        sha.update(text.as_bytes());
+        sha.finalize_reset()
     });
 
-    fmt::format(format_args!("{:X}", result))
+
+    let data: [u8; 32] = result.into();
+
+    // ASCII-Hex encode the data.
+    // This is equivalent to write!(some_str, "{:X}", &data[..]);
+    // but avoids the need for a heap-allocated string buffer.
+    for (idx, byte) in data.into_iter().enumerate() {
+        // NOTE(unwrap): u8 & 0xF is always valid.
+        let lhs = hex_encode((byte >> 4) & 0xF).unwrap();
+        let rhs = hex_encode(byte & 0xF).unwrap();
+
+        buffer[idx * 2] = lhs;
+        buffer[(idx * 2) + 1] = rhs;
+    }
+
+    buffer
+}
+
+pub(crate) fn hex_encode(value: u8) -> Option<u8> {
+    if value < 10 {
+        Some(value.wrapping_add(b'0'))
+    } else if value < 16 {
+        Some(value.wrapping_sub(10).wrapping_add(b'A'))
+    } else {
+        None
+    }
 }
