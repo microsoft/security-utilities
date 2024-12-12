77e5c6e6579108040eb8cfcadf02cfabc0686cfe
Joel Verhagen <jver@microsoft.com>
2024-12-12 11:56:34 -0500

    Add more true positive examples

--- src/Microsoft.Security.Utilities.Core/PreciselyClassifiedSecurityKeys/SEC101_031_NuGetApiKey.cs
+++ src/Microsoft.Security.Utilities.Core/PreciselyClassifiedSecurityKeys/SEC101_031_NuGetApiKey.cs
@@ -7,11 +7,13 @@ namespace Microsoft.Security.Utilities
 {
     public class NuGetApiKey : RegexPattern
     {
+        private const string Base32 = "abcdefghijklmnopqrstuvwxyz234567";
+
         public NuGetApiKey()
         {
             Id = "SEC101/031";
             Name = nameof(NuGetApiKey);
-            DetectionMetadata = DetectionMetadata.FixedSignature | DetectionMetadata.HighEntropy| DetectionMetadata.HighConfidence;
+            DetectionMetadata = DetectionMetadata.FixedSignature | DetectionMetadata.HighEntropy | DetectionMetadata.HighConfidence;
 
             // This is the ApiKeyV4 format implemented here:
             // https://github.com/NuGet/NuGetGallery/blob/main/src/NuGetGallery.Services/Authentication/ApiKeyV4.cs
@@ -22,7 +24,14 @@ namespace Microsoft.Security.Utilities
 
         public override IEnumerable<string> GenerateTruePositiveExamples()
         {
+            // matches the older, GUID-based entropy source
             yield return $"oy2a{WellKnownRegexPatterns.RandomLowercase(15)}a{WellKnownRegexPatterns.RandomLowercase(11)}e7a{WellKnownRegexPatterns.RandomLowercase(11)}a";
+
+            // matches the newer, PRNG-based entropy source
+            yield return $"oy2{WellKnownRegexPatterns.GenerateString(Base32, 43)}";
+
+            // repeat a single base32 character 43 times, for an obviously contrived example
+            yield return $"oy2{new string(WellKnownRegexPatterns.GenerateString(Base32, 1)[0], 43)}";
         }
     }
 }
