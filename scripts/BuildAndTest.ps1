f0b718f2672c96e7e338677a0799fa131409d755
Nick Guerrera <nguerrera@microsoft.com>
2025-01-30 15:32:20 -0600

    build: speed up BuildAndTest.cmd (#123)
    `BuildAndTest` was terribly slow and basically unusable in any inner loop. The main culprit was that `cargo clean` was done on every invocation. This causes the rust world to be rebuilt every single time, and that is quite slow. There were some other issues that are also fixed here.
    
    Changes:
    * Remove all `cargo clean` calls. Either use `git clean` or run `cargo clean` yourself if you want to rebuild. This is consistent with the C# behavior of `BuildAndTest`.
    * Stop doing `cargo test` on the `ffi` directory as this builds stuff again unnecessarily but runs no additional tests.
    * Extract a separate BuildAndTestRust.cmd script to allow iteration on rust changes without running C# build and test steps.
    * Respect `-NoTest` argument for rust.
    * Clean up some build error handling and logging output.
    * Remove dead `rd /s /q` code. If you had a `<repo>\..\bld`, which you usually don't, it would fail because this is cmd not powershell syntax. Furthermore, blowing away `bld` n every invocation runs counter to the goal here of speeding things up for iteration, so it is better to delete this thing that never did anything than to fix it.

--- scripts/BuildAndTest.ps1
+++ scripts/BuildAndTest.ps1
@@ -51,11 +51,6 @@ function Exit-WithFailureMessage($scriptName, $message) {
     exit 1
 }
 
-If (Test-Path "..\bld") {
-    Write-Information "Deleting old build..."
-    rd /s /q ..\bld
-}
-
 if (-not $NoBuild) {
     Write-Information "Building Microsoft.Security.Utilities.sln (dotnet)..."
     dotnet build $RepoRoot\src\Microsoft.Security.Utilities.sln -c $Configuration -p:Deterministic=true -p:WarningsAsErrors="MSB3277"
@@ -96,22 +91,24 @@ if ($LASTEXITCODE -ne 0) {
     Exit-WithFailureMessage $ScriptName "Build of SecurityUtilitiesPackageReference failed."
 }
 
-Write-Information "Running API examples using compiled Microsoft.Security.Utilities.Core package on net451..."
-Invoke-Expression "$RepoRoot\src\SecurityUtilitiesPackageReference\SecurityUtilitiesApiUtilizationExample\bin\$Configuration\net451\SecurityUtilitiesApiUtilizationExample.exe"
-if ($LASTEXITCODE -ne 0) {
-    Exit-WithFailureMessage $ScriptName "Microsoft.Security.Utilities.Core API example execution failed."
-}
+if (-not $NoTest) {
+    Write-Information "Running API examples using compiled Microsoft.Security.Utilities.Core package on net451..."
+    Invoke-Expression "$RepoRoot\src\SecurityUtilitiesPackageReference\SecurityUtilitiesApiUtilizationExample\bin\$Configuration\net451\SecurityUtilitiesApiUtilizationExample.exe"
+    if ($LASTEXITCODE -ne 0) {
+        Exit-WithFailureMessage $ScriptName "Microsoft.Security.Utilities.Core API example execution failed."
+    }
 
-Write-Information "Running API examples using compiled Microsoft.Security.Utilities.Core package on net462..."
-Invoke-Expression "$RepoRoot\src\SecurityUtilitiesPackageReference\SecurityUtilitiesApiUtilizationExample\bin\$Configuration\net462\SecurityUtilitiesApiUtilizationExample.exe"
-if ($LASTEXITCODE -ne 0) {
-    Exit-WithFailureMessage $ScriptName "Microsoft.Security.Utilities.Core API example execution failed."
-}
+    Write-Information "Running API examples using compiled Microsoft.Security.Utilities.Core package on net462..."
+    Invoke-Expression "$RepoRoot\src\SecurityUtilitiesPackageReference\SecurityUtilitiesApiUtilizationExample\bin\$Configuration\net462\SecurityUtilitiesApiUtilizationExample.exe"
+    if ($LASTEXITCODE -ne 0) {
+        Exit-WithFailureMessage $ScriptName "Microsoft.Security.Utilities.Core API example execution failed."
+    }
 
-Write-Information "Running API examples using compiled Microsoft.Security.Utilities.Core package on net6.0..."
-Invoke-Expression "$RepoRoot\src\SecurityUtilitiesPackageReference\SecurityUtilitiesApiUtilizationExample\bin\$Configuration\net6.0\SecurityUtilitiesApiUtilizationExample.exe"
-if ($LASTEXITCODE -ne 0) {
-    Exit-WithFailureMessage $ScriptName "Microsoft.Security.Utilities.Core API example execution failed."
+    Write-Information "Running API examples using compiled Microsoft.Security.Utilities.Core package on net6.0..."
+    Invoke-Expression "$RepoRoot\src\SecurityUtilitiesPackageReference\SecurityUtilitiesApiUtilizationExample\bin\$Configuration\net6.0\SecurityUtilitiesApiUtilizationExample.exe"
+    if ($LASTEXITCODE -ne 0) {
+        Exit-WithFailureMessage $ScriptName "Microsoft.Security.Utilities.Core API example execution failed."
+    }
 }
 
 Write-Information "$ScriptName SUCCEEDED."
