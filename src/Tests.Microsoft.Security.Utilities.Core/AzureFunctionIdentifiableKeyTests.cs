5d2244eb419ef915e76c4f1aace94e634501d591
Michael C. Fanning <mikefan@microsoft.com>
2025-03-04 12:15:02 -0800

    Add test file.

---
+++ src/Tests.Microsoft.Security.Utilities.Core/AzureFunctionIdentifiableKeyTests.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using FluentAssertions;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Linq;
+
+
+namespace Microsoft.Security.Utilities.Core
+{
+    [TestClass]
+    public class AzureFunctionIdentifiableKeyTests
+    {
+        [TestMethod]
+        public void AzureFunctionIdentifiableKey_GenerateTruePositiveExamples()
+        {
+            var classifier = new AzureFunctionIdentifiableKey();
+
+            foreach (string example in classifier.GenerateTruePositiveExamples())
+            {
+                var masker = new SecretMasker([classifier]);
+                var detection = masker.DetectSecrets(example).FirstOrDefault();
+                detection.Should().NotBe(default);
+
+                string refined = example.Substring(detection.Start, detection.Length);
+                var result = classifier.GetMatchIdAndName(refined);
+                result.Should().NotBeNull();
+            }
+        }
+    }
+}
+
+
