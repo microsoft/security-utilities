cd9947fd52f92228918d1f47575ad6781da6eb01
Michael C. Fanning <mikefan@microsoft.com>
2025-04-02 08:54:11 -0700

    Restore 200 (CASK legacy) label name

--- src/Microsoft.Security.Utilities.Core/PreciselyClassifiedSecurityKeys/SEC101_200_UnclassifiedLegacyCommonAnnotatedSecurityKey.cs
+++ src/Microsoft.Security.Utilities.Core/PreciselyClassifiedSecurityKeys/SEC101_200_UnclassifiedLegacyCommonAnnotatedSecurityKey.cs
@@ -17,6 +17,7 @@ namespace Microsoft.Security.Utilities
             DetectionMetadata = DetectionMetadata.Identifiable;
             Pattern = $"{WellKnownRegexPatterns.PrefixBase62}(?P<refine>[{WellKnownRegexPatterns.Base62}]{{52}}JQQJ9(?:9|D|H)[{WellKnownRegexPatterns.Base62}][A-L][{WellKnownRegexPatterns.Base62}]{{16}}[A-Za-z][{WellKnownRegexPatterns.Base62}]{{7}}(?:[{WellKnownRegexPatterns.Base62}]{{2}}==)?)";
             Signatures = Signature.ToSet();
+            Label = "an unclassified legacy common annotated security key";
         }
 
 #if HIGH_PERFORMANCE_CODEGEN
