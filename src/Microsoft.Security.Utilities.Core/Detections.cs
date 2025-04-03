8e4440cbd3730170b6fad87eddadb64278df3bc2
Michael C. Fanning <mikefan@microsoft.com>
2025-04-03 12:31:09 -0700

    Add tests for truncation in Detections static helper.

--- src/Microsoft.Security.Utilities.Core/Detections.cs
+++ src/Microsoft.Security.Utilities.Core/Detections.cs
@@ -3,6 +3,8 @@
 
 global using static Microsoft.Security.Utilities.Detections;
 
+using System;
+
 namespace Microsoft.Security.Utilities
 {
     public static class Detections
@@ -39,15 +41,35 @@ namespace Microsoft.Security.Utilities
 
         /// <summary>
         /// Truncates a string to a specified length, adding an ellipsis if any
-        /// of the input string is removed.
+        /// of the input string is removed. This helper will remove up to two
+        /// trailing equals signs from the input string before truncating it.
+        /// This is intended to help cover the standard case where a secret is
+        /// base64-encoded data.
         /// </summary>
         /// <param name="text">The text to truncate.</param>
-        /// <param name="lengthExclusiveOfEllipsis">The desired length of the truncated text, exclusive of the ellipsis, if added.</param>
-        /// <returns>The rightmost truncated contents of the strength or the entire string (if its length is equal to or less than the specified length).</returns>
+        /// <param name="lengthExclusiveOfEllipsis">The desired length of the
+        /// truncated text, exclusive of the ellipsis, if added.</param>
+        /// <returns>The rightmost truncated contents of the strength or the
+        /// entire string (if its length is equal to or less than the specified
+        /// length).</returns>
         public static string TruncateSecret(string text, int lengthExclusiveOfEllipsis = 6)
         {
             text ??= string.Empty;
-            string truncatedText = text.TrimEnd('=');
+            string truncatedText = text.Replace("-", "+").Replace("_", "/");
+
+            try
+            {
+                // If we receive valid base64 or url-safe base64, we will
+                // elide any trailing equal signs, so that the padding that's
+                // retained in the truncated strength does not apply to the 
+                // length constraint. 
+                Convert.FromBase64String(truncatedText);
+                truncatedText = text.TrimEnd('=');
+            }
+            catch (FormatException)
+            {
+            }
+
             string suffix = new string('=', text.Length - truncatedText.Length);
 
             if (truncatedText.Length <= lengthExclusiveOfEllipsis)
