f8c58aef77ac9efe752ab4b3c71a4f4a3f0e5f14
Michael C. Fanning <mikefan@microsoft.com>
2025-05-07 14:10:20 -0700

    Add new rules and emitted JSON metadata

---
+++ src/Microsoft.Security.Utilities.Core/PreciselyClassifiedSecurityKeys/SEC101_197_AzureAppConfigurationLegacyCommonAnnotatedSecurityKey.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+
+namespace Microsoft.Security.Utilities
+{
+    public class AzureAppConfigurationLegacyCommonAnnotatedSecurityKey : LegacyCommonAnnotatedSecurityAccessKey
+    {
+        public AzureAppConfigurationLegacyCommonAnnotatedSecurityKey() : base()
+        {
+            Id = "SEC101/197";
+            Name = nameof(AzureAppConfigurationLegacyCommonAnnotatedSecurityKey);
+            Label = "an Azure App Configuration legacy common annotated security key";
+        }
+
+        protected override string ProviderSignature => LegacyCaskProviderSignatures.AzureAppConfiguration;
+
+        public override Version CreatedVersion => Releases.Unreleased;
+    }
+}
---
+++ src/Microsoft.Security.Utilities.Core/PreciselyClassifiedSecurityKeys/SEC101_203_AzureMapsLegacyCommonAnnotatedSecurityKey.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+
+namespace Microsoft.Security.Utilities
+{
+    public class AzureMapsLegacyCommonAnnotatedSecurityKey : LegacyCommonAnnotatedSecurityAccessKey
+    {
+        public AzureMapsLegacyCommonAnnotatedSecurityKey() : base()
+        {
+            Id = "SEC101/203";
+            Name = nameof(AzureMapsLegacyCommonAnnotatedSecurityKey);
+            Label = "an Azure Maps legacy common annotated security key";
+        }
+
+        protected override string ProviderSignature => LegacyCaskProviderSignatures.AzureMaps;
+
+        public override Version CreatedVersion => Releases.Unreleased;
+    }
+}
